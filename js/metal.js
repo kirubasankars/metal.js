325d558cda4bc55a18eeea805f637977206ee767
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2015-12-10 12:00:12 -0500

    fixes for toJSON  and added testcases

--- js/metal.js
+++ js/metal.js
@@ -1,174 +1,161 @@
-(function(scope) {
+(function (scope) {
 
-  function metal() {    
-    this._attributes = {};
-	this._length = 0;	
-	this._array = false;
-	
-    this._observers = {};
-  }
+    function metal() {
+        this._attributes = {};
+        this._length = 0;
+        this._array = false;
 
-  metal.prototype.trigger = function(key) {
-    var observers = this._observers[key];
-    if (!observers) return;
-    for (idx in observers) {
-      var callee = observers[idx];
-      callee.cb.call((callee.context || this));
-    }
-  }
-
-  metal.prototype.on = function(key, cb, context) {
-    var allObservers = this._observers,
-      observers = this._observers[key];
-
-    if (!observers) {
-      allObservers[key] = observers = [];
+        this._observers = {};
     }
 
-    var idx = observers.push({
-      cb: cb,
-      context: context
-    });
-
-    return function() {
-      allObservers[key] = observers.splice(idx, 1);
-      if (allObservers[key].length === 0) {
-        delete allObservers[key];
-      }
-    }
-  }
-
-  metal.prototype.set = function(key, value) {
-    property_set.call(this, key, value);
-    if (key.indexOf('.') === -1) {
-      this._eventBubble(key);
-    }
-  }
-
-  metal.prototype._eventBubble = function(key) {
-    if (this._observers[key]) {
-      this.trigger(key);
+    metal.prototype.trigger = function (key) {
+        var observers = this._observers[key];
+        if (!observers) return;
+        for (idx in observers) {
+            var callee = observers[idx];
+            callee.cb.call((callee.context || this));
+        }
     }
 
-    if (this._parent) {
-      this._parent._eventBubble(this._name + '.' + key);
+    metal.prototype.on = function (key, cb, context) {
+        var allObservers = this._observers,
+          observers = this._observers[key];
+
+        if (!observers) {
+            allObservers[key] = observers = [];
+        }
+
+        var idx = observers.push({
+            cb: cb,
+            context: context
+        });
+
+        return function () {
+            allObservers[key] = observers.splice(idx, 1);
+            if (allObservers[key].length === 0) {
+                delete allObservers[key];
+            }
+        }
     }
-  }
 
-  metal.prototype.contains = function(key) {
-    return this._attributes[key] !== undefined;
-  }
+    metal.prototype.set = function (key, value) {
+        property_set.call(this, key, value);        
+    }
 
-  metal.prototype.get = function(key) {
-    return property_get.call(this, key);
-  }
+    metal.prototype.contains = function (key) {
+        return this._attributes[key] !== undefined;
+    }
 
-  metal.prototype.toJSON = function() {
-    var output = {},
-      prop, value;
+    metal.prototype.get = function (key) {
+        return property_get.call(this, key);
+    }
 
-    for (prop in this._attributes) {
-      value = this._attributes[prop];
-      if (value instanceof metal) {
-        if (value._array === true) {
-          value = value.toJSON();
+    metal.prototype.toJSON = function () {
+        var output = {},
+          prop, value;
+
+        for (prop in this._attributes) {
+            value = this._attributes[prop];
+            if (value instanceof metal) {
+                if (value._array === true) {
+                    value = value.toJSON();
+                } else {
+                    value = value._attributes;
+                }
+            }
+            if (this._array) {
+                prop = prop.substr(1, prop.length);
+            }
+            output[prop] = value;
+        }
+
+        return output;
+    }
+
+    function toJSON() {
+        if (this._array) {
+            toJSON.call(this)
+        }
+        return this._attributes;
+    }
+
+    function property_get(property) {
+        var dot = property.indexOf('.'),
+            path, remainingPath;
+        if (typeof property === "string" && dot > -1) {
+            path = property.substr(0, dot);
+            remainingPath = property.substr(dot + 1);
+
+            if (this.contains(path) === true) {
+                var child = this.get(path);
+                if (child === null) throw path + " is not exists.";
+                return child.get(remainingPath);
+            }
+
+            if (path === "$parent") {
+                if (this._parent._array === true) {
+                    return this._parent._parent.get(remainingPath);
+                }
+                return this._parent.get(remainingPath);
+            }
+
+            if (path === "$length") {
+                return this._length;
+            }
+
+            return null;
         } else {
-          value = value._attributes;
+
+            if (property === "$parent") {
+                if (this._parent._array === true) {
+                    return this._parent._parent;
+                }
+                return this._parent;
+            }
+
+            if (property === "$length") {
+                return this._length;
+            }
+
+            if (this.contains(property) === true) return this._attributes[property];
+            return null;
         }
-      }
-      if (this._array) {
-        prop = prop.substr(1, prop.length);
-      }
-      output[prop] = value;
     }
 
-    return output;
-  }
+    function property_set(property, value) {
+        var dot = property.indexOf('.'),
+            path, remainingPath, pathValue;
+        if (typeof property === "string" && dot > -1) {
+            path = property.substr(0, dot);
+            remainingPath = property.substr(dot + 1);
+            pathValue = this.get(path);
+            if (!pathValue) {
+                pathValue = new metal();
+                this.set(path, pathValue);
+            }
+            if (typeof pathValue === "object" && pathValue instanceof metal) {
+                pathValue.set(remainingPath, value);
+            }
+        } else {
 
-  function toJSON() {
-    if (this._array) {
-      toJSON.call(this)
-    }
-    return this._attributes;
-  }
+            if (property.charAt(0) === '@') {
+                path = property.substr(1, property.length - 1);
+                if (!isNaN(parseInt(path))) {
+                    this._array = true;
+                } else {
+                    throw "its a array. you try update value with " + localKey;
+                }
+            }
 
-  function property_get(property) {
-    var dot = property.indexOf('.'),
-      key, remainingKey;
-    if (typeof property === "string" && dot > -1) {
-      key = property.substr(0, dot);
-      remainingKey = property.substr(dot + 1);
+            if (value instanceof metal) {
+                value._parent = this;
+            }
 
-      if (this.contains(key) === true) {
-        var child = this.get(key);
-        if (child === null) throw key + " is not exists.";
-        return child.get(remainingKey);
-      }
-
-      if (key === "$parent") {
-        if (this._parent._array === true) {
-          return this._parent._parent.get(remainingKey);
+            this._attributes[property] = value;
+            this._length = this._length + 1;
         }
-        return this._parent.get(remainingKey);
-      }
-
-      if (key === "$length") {
-        return this._length;
-      }
-
-      return null;
-    } else {		
-
-	  if (property === "$parent") {
-        if (this._parent._array === true) {
-          return this._parent._parent;
-        }
-        return this._parent;
-      }
-	
-      if (property === "$length") {
-        return this._length;
-      }
-	  
-      if (this.contains(property) === true) return this._attributes[property];
-      return null;
     }
-  }
 
-  function property_set(property, value) {
-    var dot = property.indexOf('.'),
-		key, remainingKey, localKey, localValue;
-    if (typeof property === "string" && dot > -1) {
-      key = property.substr(0, dot);
-      remainingKey = property.substr(dot + 1);
-      if (this.contains(key) === false) {
-        this.set(key, new metal());
-      }
-      localValue = this.get(key);
-      if (typeof localValue === "object" && localValue instanceof metal) {
-        localValue.set(remainingKey, value);
-      }
-    } else {
-		
-	  if (property.charAt(0) === '@') {
-		 localKey = property.substr(1, property.length - 1);
-		 if (!isNaN(parseInt(localKey))) {
-			this._array = true;
-		 } else {
-			 throw "its a array. you try update value with " + localKey;
-		 }
-	  }
-
-      if (value instanceof metal) {
-        value._parent = this;
-        value._name = property;
-      }	  
-	  
-      this._attributes[property] = value;
-      this._length = this._length + 1;
-    }
-  }
-
-  scope.metal = metal;
+    scope.metal = metal;
 
 }(window))
--- js/metal_test.js
+++ js/metal_test.js
@@ -1,77 +1,93 @@
-QUnit.test( "set/get", function( assert ) {
-	var m = new metal();
-	m.set("name", "Test")
-	assert.ok(m._attributes.name === "Test", "exists on _attributes" );
-	assert.ok(m._length === 1, "length got updated" );
-	assert.ok(m.get("name") === "Test", "able to get value back" );	
+QUnit.test("set/get", function (assert) {
+    var m = new metal();
+    m.set("name", "Test")
+    assert.ok(m._attributes.name === "Test", "exists on _attributes");
+    assert.ok(m._length === 1, "length got updated");
+    assert.ok(m.get("name") === "Test", "able to get value back");
 });
 
-QUnit.test( "set/get level 1", function( assert ) {
-	var m = new metal();
-	m.set("name.first", "first")
-	m.set("name.last", "last")
-	var r = (m.get("name.first") === "first") && (m.get("name.last") === "last")
-	assert.ok(m._attributes.name._attributes.first === "first", "exists on _attributes" );
-	assert.ok(m._attributes.name._attributes.last === "last", "exists on _attributes" );
-	assert.ok(m._length === 1 && m._attributes.name._length === 2, "length got updated" );
-	assert.ok(r , "able to get value back" );	
+QUnit.test("set/get level 1", function (assert) {
+    var m = new metal();
+    m.set("name.first", "first")
+    m.set("name.last", "last")
+    var r = (m.get("name.first") === "first") && (m.get("name.last") === "last")
+    assert.ok(m._attributes.name._attributes.first === "first", "exists on _attributes");
+    assert.ok(m._attributes.name._attributes.last === "last", "exists on _attributes");
+    assert.ok(m._length === 1 && m._attributes.name._length === 2, "length got updated");
+    assert.ok(r, "able to get value back");
 });
 
-QUnit.test( "set/get level 2", function( assert ) {
-	var m = new metal();
-	m.set("student.name.first", "first")
-	m.set("student.name.last", "last")
-	var r = m.get("student.name.first") === "first" && m.get("student.name.last") === "last";
-	assert.ok(m._length === 1 && m._attributes.student._attributes.name._length === 2, "length got updated");
-	assert.ok(m._attributes.student._attributes.name._attributes.first === "first", "exists on _attributes" );	
-	assert.ok(r , "able to get value back" );
+
+QUnit.test("set/get level 2", function (assert) {
+    var m = new metal();
+    m.set("student.name.first", "first")
+    m.set("student.name.last", "last")
+    var r = m.get("student.name.first") === "first" && m.get("student.name.last") === "last";
+    assert.ok(m._length === 1 && m._attributes.student._attributes.name._length === 2, "length got updated");
+    assert.ok(m._attributes.student._attributes.name._attributes.first === "first", "exists on _attributes");
+    assert.ok(r, "able to get value back");
 });
 
-QUnit.test( "array set/get simple value", function( assert ) {
-	var m = new metal();
-	m.set("student.marks.@0", 100)
-	m.set("student.marks.@2", 95)
-	var r = m.get("student.marks.@0") === 100 && m.get("student.marks.@2") === 95 
+QUnit.test("array set/get simple value", function (assert) {
+    var m = new metal();
+    m.set("student.marks.@0", 100)
+    m.set("student.marks.@2", 95)
+    var r = m.get("student.marks.@0") === 100 && m.get("student.marks.@2") === 95
 			&& m._attributes.student._attributes.marks._array;
-	assert.ok(r , "able to get value back" );
+    assert.ok(r, "able to get value back");
 });
 
-QUnit.test( "array set/get simple and object value", function( assert ) {
-	var m = new metal();
-	m.set("student.marks.@0", 100)
-	m.set("student.marks.@2", 95)
-	m.set("student.marks.@3.value", 45)
-	var r = m.get("student.marks.@0") === 100 && m.get("student.marks.@2") === 95 && m.get("student.marks.@3.value") === 45
-	assert.ok(r , "able to get value back" );
+QUnit.test("array set/get simple and object value", function (assert) {
+    var m = new metal();
+    m.set("student.marks.@0", 100)
+    m.set("student.marks.@2", 95)
+    m.set("student.marks.@3.value", 45)
+    var r = m.get("student.marks.@0") === 100 && m.get("student.marks.@2") === 95 && m.get("student.marks.@3.value") === 45
+    assert.ok(r, "able to get value back");
 });
 
-QUnit.test( "set/get level 2 return metal", function( assert ) {
-	var m = new metal();
-	m.set("student.name.first", "first")	
-	var r1 = m.get("student.name") instanceof metal
-	var r = m.get("student.name").get('first') === "first" && r1
-	assert.ok(r , "returns metal object" );
+QUnit.test("set/get level 2 return metal", function (assert) {
+    var m = new metal();
+    m.set("student.name.first", "first")
+    var r1 = m.get("student.name") instanceof metal
+    var r = m.get("student.name").get('first') === "first" && r1
+    assert.ok(r, "returns metal object");
 });
 
-QUnit.test( "get $length", function( assert ) {
-	var m = new metal();
-	m.set("student.name.first", "first")
-	m.set("student.name.last", "last")	
-	var r = m.get("student.name.$length") === 2
-	assert.ok(r , "$length works");
+QUnit.test("get $length", function (assert) {
+    var m = new metal();
+    m.set("student.name.first", "first")
+    m.set("student.name.last", "last")
+    var r = m.get("student.name.$length") === 2
+    assert.ok(r, "$length works");
 });
 
-QUnit.test( "get $parent", function( assert ) {
-	var m = new metal();
-	m.set("student.name.first", "first")	
-	var r = (m.get("student.name.$parent") === m.get("student"))
-	assert.ok(r , "$parent works");
+QUnit.test("set/get $parent", function (assert) {
+    var m = new metal();
+    m.set("student.name.first", "first")
+    debugger;
+    m.set("student.name.$parent.age", 32)
+    var r = (m.get("student.name.$parent") === m.get("student")) && m.get('student.name.$parent.age') === 32
+    assert.ok(r, "$parent works");
 });
 
-QUnit.test( "exception when array overriding", function( assert ) {
-	var student = new metal();
-	student.set("marks.@0", 100)
-	student.set("marks.@1", 80)	
-	assert.ok(student._attributes.marks._array, "is array");
-	assert.throws(function() {student.set("marks.@a", 80)},"its a array. you try update value without index.");	
+QUnit.test("exception when array overriding", function (assert) {
+    var student = new metal();
+    student.set("marks.@0", 100)
+    student.set("marks.@1", 80)
+    assert.ok(student._attributes.marks._array, "is array");
+    assert.throws(function () { student.set("marks.@a", 80) }, "its a array. you try update value without index.");
+});
+
+QUnit.test("toJSON", function (assert) {
+    var m = new metal();
+    m.set("student.name.first", "Test")
+    m.set("student.name.last", "Test")
+    m.set("student.name.$parent.phone", "99999999")
+    m.set("student.marks.@0", 100)
+    m.set("student.marks.@2", 95)
+    m.set("student.marks.@3.value", 95)
+    var v = JSON.stringify(m.toJSON());
+    console.log(v)
+    assert.ok(v === '{"student":{"name":{"first":"Test","last":"Test"},"phone":"99999999","marks":{"0":100,"2":95,"3":{"value":95}}}}')
 });
